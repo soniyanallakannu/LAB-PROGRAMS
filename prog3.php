03f6894dfc138a001074984554a238a607d59b1f
soniya <soniyanallakannu@gmail.com>
2025-09-17 22:47:11 +0530

    prog3.php

--- prog3.php
+++ prog3.php
@@ -1,42 +1,48 @@
 <!DOCTYPE html>
 <html>
 <body>
-<center><b><h1> String handling and reversing</h1></b></center>
-<form action="pro4.php" method="get"> ENTER YOUR CHOICE<input type="text" name="CHOICE"/><br><br>
-<input type="submit" value="Submit"/><br>
+<center><b><h1>String Handling and Reversing</h1></b></center>
+
+<form action="" method="get">
+    ENTER YOUR CHOICE: <input type="text" name="CHOICE"/><br><br>
+    <input type="submit" value="Submit"/><br>
 </form>
+
 <?php
-isset($_GET["CHOICE"]);
-$n=$_GET["CHOICE"];
-function countt($n)
-{
-//$g=$n;
-$upper=0;
-$lower=0;
-$number=0;
-$special=0;
-//$a=[];
-for($i=0;$i<strlen($n);$i++)
-{
-if(ctype_upper($n[$i]))
-$upper++;
-elseif(ctype_lower($n[$i]))
-$lower++;
-elseif(ctype_digit($n[$i]))
-$number++; else
-$special++;
-}
-echo "Upper case letters: ".$upper; echo "<br>";
-echo "Lowercase letters: ".$lower; echo "<br>";
-echo "Number: ".$number; echo "<br>";
-echo "special charcter: ".$special; echo "<br>";
+// Run only when form is submitted and not empty
+if (isset($_GET["CHOICE"]) && $_GET["CHOICE"] !== '') {
+    $n = $_GET["CHOICE"];
 
-}
-countt($n);
+    function countt($n) {
+        $upper = 0;
+        $lower = 0;
+        $number = 0;
+        $special = 0;
 
+        for ($i = 0; $i < strlen($n); $i++) {
+            $ch = $n[$i];
+            if (ctype_upper($ch)) {
+                $upper++;
+            } elseif (ctype_lower($ch)) {
+                $lower++;
+            } elseif (ctype_digit($ch)) {
+                $number++;
+            } else {
+                $special++;
+            }
+        }
+
+        echo "Upper case letters: " . $upper . "<br>";
+        echo "Lowercase letters: " . $lower . "<br>";
+        echo "Number: " . $number . "<br>";
+        echo "Special characters: " . $special . "<br>";
+
+        // Also show reversed string
+        echo "Reversed string: " . strrev($n) . "<br>";
+    }
+
+    countt($n);
+}
 ?>
 </body>
-</html> 
-
-
-
+</html>
